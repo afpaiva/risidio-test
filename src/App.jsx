750c56556516804cb0b7bf81eaefb7e598715850
afpaiva <andrepaiva.arq@gmail.com>
2024-04-10 11:32:57 -0300

    added a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./components/scene";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Physics } from "@react-three/rapier";
 import { Environment } from "@react-three/drei";
 import { Modal } from "./ui/modal";
 import { ToolsBar } from "./ui/tools-bar";
 import { Logo } from "./ui/logo";
 import { Bloom } from "@react-three/postprocessing";
+import { Loader } from "@react-three/drei";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoaded = () => {
+    setLoading(false);
+  };
+
   return (
     <>
+      {loading && <Loader />}
       <Logo />
       <Modal />
       <ToolsBar />
       <Canvas className="canvas-scene">
         <Suspense fallback={null}>
           <Physics>
-            <Scene />
+            <Scene onLoaded={handleLoaded} />
           </Physics>
           <Environment preset="city" background />
           <Bloom
